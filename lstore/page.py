6cf28582576abe5ea9df8c4818725c14c180e10a
Matthew Fulde <mpfulde@ucdavis.edu>
2024-02-08 01:17:44 -0800

    added skeleton for pagerange setup

--- lstore/page.py
+++ lstore/page.py
@@ -1,4 +1,7 @@
 
+MAX_PAGES = 8
+RECORD_COLUMNS = 4 # 4 constant columns for all tables (definey in lstore/table.py)
+
 class Page:
 
     def __init__(self):
@@ -8,7 +11,17 @@ class Page:
     def has_capacity(self):
         pass
 
+    # outside of setup, exclusive to tail page
     def write(self, value):
         self.num_records += 1
         pass
+
+class PageRange:
+
+    def __init__(self, num_columns, key)
+        self.BasePages = [Page() for i in range(MAX_PAGES)]
+
+        # add logic to link BasePages and connect a tailpage
+
+        self.TailPages = Page()
 
