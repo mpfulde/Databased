0dda45f4cc2aef8f977843af245e693c3fea2eb6
Matthew Fulde <mpfulde@ucdavis.edu>
2024-02-08 20:34:42 -0800

    added write record for basepages to pages

--- lstore/page.py
+++ lstore/page.py
@@ -1,35 +1,76 @@
+from table import Record
 
-MAX_PAGES = 8
-RECORD_COLUMNS = 4 # 4 constant columns for all tables (definey in lstore/table.py)
+NO_BASE_PAGES = 4  # 4 constant columns for all tables (definey in lstore/table.py)
 NO_BYTES = 1
+
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(4096)
+        # if we run out of space we want to have another page that links back to the original (in the same column)
+        self.parent = None
+        self.child = None
+        self.child_index = 0
 
+    def has_child(self):
+        return self.child is not None
+
+    # if there are more records than available bytes return False
     def has_capacity(self):
-        pass
+        return self.num_records * NO_BYTES < 4096
 
-    # outside of setup, exclusive to tail page
-    # unsure of the no. of bytes the value needs
     def write(self, value):
-        self.data.append(value.to_bytes(NO_BYTES, byteorder='big'))
+        if not self.has_capacity():
+            return False
+
+        self.data[self.num_records * NO_BYTES] = value.to_byte(NO_BYTES, 'big')
         self.num_records += 1
         pass
 
-    def read(self, location):
+    # converts a record into a usable array to write to the data array
 
-        value = int.from_bytes(self.data[location:(location + NO_BYTES)], byteorder='big')
-        return value
 
 class PageRange:
 
-    def __init__(self, num_columns, key)
-        self.BasePages = [Page() for i in range(MAX_PAGES)]
+    def __init__(self, num_columns):
+        # will regret later but for now just storing all base pages in a list its easier although slower
+        self.BasePages = [Page() for i in range(NO_BASE_PAGES)]
 
-        # add logic to link BasePages and connect a tailpage
+        self.tailID = NO_BASE_PAGES
 
-        self.TailPages = Page()
+        # assigns the parent to
+        self.TailPages = [Page()] * num_columns
 
+    def createNewBasePage(self, parent):
+        basePage = Page()
+        basePage.parent = parent
+        return basePage
+
+    def write_record(self, record):
+        record_list = record.createList()
+        successful_write = False
+        for i in range(NO_BASE_PAGES):
+
+            if i >= NO_BASE_PAGES:
+                raise Exception("Outside of allowed column space")
+
+            index = i
+
+            # gets the latest page (where there is no child)
+            while self.BasePages[index].has_child():
+                index = self.BasePages[index].child_index
+
+            if not self.BasePages[i].has_capacity() and not self.BasePages[i].has_child():
+                self.BasePages[i].child_index = len(self.BasePages)
+                new_base = self.createNewBasePage(self.BasePages[i])
+                self.BasePages[i].child = new_base
+
+            successful_write = self.BasePages[index].write(record_list[i])
+
+            if not successful_write:
+                raise Exception("Something went wrong and it didnt happen in the write function")
+
+        # if successful return True, if unsuccessful will throw exception
+        return successful_write
