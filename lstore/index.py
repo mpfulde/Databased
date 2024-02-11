552f32686246e3020a6485ef216e0ff94320e1e0
mpfulde <mpfulde@ucdavis.edu>
2024-02-11 02:32:48 -0800

    added index location function to get rid from a given value

--- lstore/index.py
+++ lstore/index.py
@@ -1,3 +1,6 @@
+from lstore.table import Table
+from lstore.page import Page, PageRange
+
 """
 A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
 """
@@ -6,14 +9,27 @@ class Index:
 
     def __init__(self, table):
         # One index for each table. All our empty initially.
-        self.indices = [None] *  table.num_columns
+        self.table = table
+        self.indices = [None] * table.num_columns
         pass
 
     """
     # returns the location of all records with the given value on column "column"
+    # will update with milestone 2, right now mapped 1 to 1
     """
 
     def locate(self, column, value):
+        for page_range in self.table.page_ranges:
+            key_page = page_range.TailPages[self.indices[column]]
+            rid_page = page_range.BasePages[3]  # this will change with indexing
+            while key_page is not None:
+                for i in range(512):  # reads each row
+                    if key_page.read(i) is value:
+                        return rid_page.read(i)
+                    else:
+                        key_page = key_page.parent
+                        rid_page = rid_page.parent
+
         pass
 
     """
@@ -28,6 +44,7 @@ class Index:
     """
 
     def create_index(self, column_number):
+
         pass
 
     """
