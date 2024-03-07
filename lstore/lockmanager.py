38d278ca5778577ca4c908b51bb8c666994809dc
Matthew Fulde <mpfulde@ucdavis.edu>
2024-03-06 20:13:51 -0800

    added lock manager to table

--- lstore/lockmanager.py
+++ lstore/lockmanager.py
@@ -7,13 +7,37 @@ class TwoPL:
 
 
     def read_lock_acquire(self):
+        self.lock.acquire()
         pass
 
     def write_lock_acquire(self):
+        self.lock.acquire()
         pass
 
     def read_lock_release(self):
+        self.lock.release()
         pass
 
     def write_lock_release(self):
+        self.lock.release()
         pass
+
+class LockManager:
+    def __init__(self):
+        self.read_list = []
+        self.write_list = []
+        pass
+
+    def release_all_reads(self):
+        for lock in self.read_list:
+            lock.read_lock_release()
+        pass
+
+    def release_all_writers(self):
+        for lock in self.write_list:
+            lock.write_lock_release()
+        pass
+
+    def release_all_locks(self):
+        self.release_all_reads()
+        self.release_all_writers()
--- lstore/table.py
+++ lstore/table.py
@@ -8,6 +8,7 @@ from time import time
 from lstore.config import *
 from lstore.index import Index
 from lstore.page import PageRange
+from lstore.lockmanager import LockManager
 
 
 def record_from_list(rlist, original):
@@ -87,6 +88,7 @@ class Table:
         self.page_directory = {}
         self.index = Index(self)
         self.bufferpool = None
+        self.lock_manager = LockManager()
 
         self.merge_lock = threading.Lock()
         self.merge_thread = None
