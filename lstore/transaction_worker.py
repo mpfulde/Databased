33a7d8198f6ee50e346b81a1acb89da08c0fb9fd
Matthew Fulde <mpfulde@ucdavis.edu>
2024-03-08 16:21:08 -0800

    fixed bug where duplicating transactions would cause an infinite loop

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -1,49 +1,50 @@
-import threading
-
 from lstore.table import Table, Record
 from lstore.index import Index
 
-class TransactionWorker:
+import threading
 
+class TransactionWorker:
     """
     # Creates a transaction worker object.
     """
-    def __init__(self, transactions = []):
+
+    def __init__(self, transactions=None):
+        if transactions is None:
+            transactions = []
+        self.thread = None
         self.stats = []
         self.transactions = transactions
         self.result = 0
-        pass
 
-    
     """
     Appends t to transactions
     """
+
     def add_transaction(self, t):
         self.transactions.append(t)
 
-        
     """
     Runs all transaction as a thread
     """
+
     def run(self):
-        self.thread = threading.Thread(target=self.__run)
+        self.thread = threading.Thread(target=self.__run, daemon=True)
         self.thread.start()
         pass
         # here you need to create a thread and call __run
-    
 
     """
     Waits for the worker to finish
     """
+
     def join(self):
         self.thread.join()
         pass
-
 
     def __run(self):
         for transaction in self.transactions:
             # each transaction returns True if committed or False if aborted
             self.stats.append(transaction.run())
+            
         # stores the number of transactions that committed
         self.result = len(list(filter(lambda x: x, self.stats)))
-
